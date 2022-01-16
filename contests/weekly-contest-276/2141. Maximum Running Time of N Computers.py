0767a54f3cd4dc40a9bcbd40e5a3301b7a5c1a9d
AsifIqbal1997 <asif.iqbal@amplussolar.com>
2022-01-16 12:36:32 +0530

    update 2141

--- contests/weekly-contest-276/2141. Maximum Running Time of N Computers.py
+++ contests/weekly-contest-276/2141. Maximum Running Time of N Computers.py
@@ -1,3 +1,15 @@
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
-        pass
+        l = 0
+        r = sum(batteries)
+        
+        def check(x):
+            return sum([min(x,b) for b in batteries]) >= n*x
+        
+        while l<=r:
+            m = (l+r)//2
+            if check(m):
+                l = m+1
+            else:
+                r = m -1
+        return r
